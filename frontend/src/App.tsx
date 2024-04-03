aeef49e48e301ee43249c159d2bc31249c0c63e1
Luiz Henrique <lhcontato2020@gmail.com>
2024-04-03 20:27:40 -0300

    feat: import Bimestre component into App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import Bimestre from './components/Bimestre'
+import { IBimestre } from './types'
 
 export default function App() {
   return (
-    <div className='h-screen text-white bg-[#0F0F0F]'>App</div>
+    <div className='lg:h-screen text-white flex justify-center'>
+      <div className='lg:max-w-[1024px] w-full p-8 gap-8'>
+          <Bimestre numeroBimestre={IBimestre.PRIMEIRO} />
+          <Bimestre numeroBimestre={IBimestre.SEGUNDO} />
+          <Bimestre numeroBimestre={IBimestre.TERCEIRO} />
+          <Bimestre numeroBimestre={IBimestre.QUARTO} />
+      </div>
+    </div>
   )
 }
