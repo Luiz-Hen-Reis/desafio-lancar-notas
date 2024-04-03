297b81f2d6620d7c94f33f763dd40a33e421d7a1
Luiz Henrique <lhcontato2020@gmail.com>
2024-04-03 20:26:29 -0300

    chore: create types

---
+++ frontend/src/types.ts
@@ -0,0 +1,20 @@
+export enum IBimestre {
+    PRIMEIRO = "PRIMEIRO",
+    SEGUNDO = "SEGUNDO",
+    TERCEIRO = "TERCEIRO",
+    QUARTO = "QUARTO"
+}
+
+export enum IDisciplina {
+    Biologia = "Biologia",
+    Artes = "Artes",
+    Geografia = "Geografia",
+    Sociologia = "Sociologia"
+}
+
+export interface Resultado {
+    id: string;
+    bimestre: IBimestre;
+    disciplina: IDisciplina;
+    nota: number;
+  }
