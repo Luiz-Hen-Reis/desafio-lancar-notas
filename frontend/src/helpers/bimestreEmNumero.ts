cc98498ff37794d745a2f615208b3288fe235d26
Luiz Henrique <lhcontato2020@gmail.com>
2024-04-04 21:13:50 -0300

    feat: create bimestreEmNumero helper

---
+++ frontend/src/helpers/bimestreEmNumero.ts
@@ -0,0 +1,8 @@
+import { IBimestre } from "../types";
+
+export const bimestreEmNumero = {
+    [IBimestre.PRIMEIRO]: 1,
+    [IBimestre.SEGUNDO]: 2,
+    [IBimestre.TERCEIRO]: 3,
+    [IBimestre.QUARTO]: 4
+  }
