ba9bad4135e9208faa9c2d0a462cd043ca3f862f
Luiz Henrique <lhcontato2020@gmail.com>
2024-04-04 21:16:36 -0300

    feat: update Disciplina component

--- frontend/src/components/Disciplina.tsx
+++ frontend/src/components/Disciplina.tsx
@@ -1,16 +1,22 @@
+import { corDisciplina } from "../helpers/corDisciplina";
+import { formatarData } from "../helpers/formatarData";
 import { IDisciplina, Resultado } from "../types"
 
 type Props = {
     disciplina: Resultado;
+    handleRemoverDisciplina: (id: string) => void;
 }
 
-export default function Disciplina({ disciplina }: Props) {
+export default function Disciplina({ disciplina, handleRemoverDisciplina }: Props) {
 
-    const corDisciplina = {
-        [IDisciplina.Artes]: "#05A2C2",
-        [IDisciplina.Biologia]: "#CC4090",
-        [IDisciplina.Geografia]: "#C26719",
-        [IDisciplina.Sociologia]: "#9B19C2",
+    let corNota = '';
+
+    if (disciplina.nota < 6) {
+        corNota = "#FF5964";
+    } else if (disciplina.nota >= 6 && disciplina.nota < 8) {
+        corNota = "#FFFF99";
+    } else {
+        corNota = "#05FF00";
     }
 
   return (
@@ -19,14 +25,14 @@ export default function Disciplina({ disciplina }: Props) {
         <div className="flex justify-between items-center">
             <div>
                 <h3>{disciplina.disciplina}</h3>
-                <p>Data</p>
+                <p>{formatarData(disciplina.criadoEm as string)}</p>
             </div>
-            <button className="cursor-pointer">
+            <button className="cursor-pointer" onClick={() => handleRemoverDisciplina(disciplina.id as string)}>
                 <img src="/assets/trash-can-icon.svg" alt="Remover disciplina" />
             </button>
         </div>
         <div>
-            <p className="bg-[#0F0F0F] flex gap-2">
+            <p style={{ color: corNota }} className="bg-[#0F0F0F] flex gap-2">
                 <img src="/assets/grade.svg" alt="Nota: " />
                 Nota: {disciplina.nota}
             </p>
