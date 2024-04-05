9279c7b68cbe0b978da00eb9a925d1888fb36117
Luiz Henrique <lhcontato2020@gmail.com>
2024-04-04 21:13:34 -0300

    feat: create corDisciplina helper

---
+++ frontend/src/helpers/corDisciplina.ts
@@ -0,0 +1,8 @@
+import { IDisciplina } from "../types";
+
+export const corDisciplina = {
+    [IDisciplina.Artes]: "#05A2C2",
+    [IDisciplina.Biologia]: "#CC4090",
+    [IDisciplina.Geografia]: "#C26719",
+    [IDisciplina.Sociologia]: "#9B19C2",
+}
